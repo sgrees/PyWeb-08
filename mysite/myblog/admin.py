d349235f928f48e037bd0ecffa228632715187aa
Susan Rees <susan@reeshome.org>
2016-05-24 14:58:10 -0700

    completed session 08 homework

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -4,5 +4,15 @@ from myblog.models import Category
 from myblog.models import Post
 
 
-admin.site.register(Category)
-admin.site.register(Post)
+class Category_Inline(admin.TabularInline):
+    model = Category.posts.through
+
+class Category_Admin(admin.ModelAdmin):
+    inlines = [Category_Inline]
+    exclude = ('posts',)
+
+class Post_Admin(admin.ModelAdmin):
+    inlines = [Category_Inline]
+
+admin.site.register(Category, Category_Admin)
+admin.site.register(Post, Post_Admin)
